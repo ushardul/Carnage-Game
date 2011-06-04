a62af33cc2e6ddb337afc6422345dcc109647029
Shardul Upadhyay <ushardul@gmail.com>
2011-06-03 22:49:15 -0400

    Removed unnecessary class files and added more information to the README.

--- Carnage/EntryPoint.java
+++ Carnage/EntryPoint.java
@@ -6,7 +6,7 @@ ICS4U1
 Date: June 13, 2010
 Due:  June 14, 2010
 
-The purpose is to start the JFrame and call the MainControl class. 
+The purpose is to start the JFrame and call the MainControl class.
 
 */
 
@@ -17,7 +17,7 @@ public class EntryPoint extends JFrame
     //EntryPoint method that is passed both players names.
     public EntryPoint (String player1, String player2)
     {
-    //adds the MainControl to the ContentPane panel.
+	//adds the MainControl to the ContentPane panel.
 	getContentPane ().add (new MainControl (player1, player2));
 	//sets panel size of 807 x 831.
 	setSize (807, 831);
--- README.txt
+++ README.txt
@@ -1,5 +1,7 @@
 Carnage was made for a Grade 12 ISU by Shardul Upadhyay, Tom Cui, and Graham McColl.
 
+Date Created: June 2010
+
 Written in Java 1.4 and the Ready to Program in Java IDE as required for the project.
 
 Carnage is a game where the objective is to destroy the other person given certain weapons. The game can be played on several maps and is very easily extendable to include further maps.
